6dea0e8925b8733505c0de30b4dac39bf9f759f5
dethurah <nicolai_wulff@hotmail.com>
2020-12-18 11:40:50 +0100

    Eksempel på Attribute

--- CsExam/Examples/4 - Assemblies, Type reflection, Late binding etc/Type_Reflection.cs
+++ CsExam/Examples/4 - Assemblies, Type reflection, Late binding etc/Type_Reflection.cs
@@ -1,80 +1,27 @@
-﻿using System;
+using System;
 using System.Reflection;
+using AssemblyExample;
 
 namespace CsExam.Examples
 {
-    public class Type_Reflection
+    public class Type_Reflection2
     {
-        // Properties definition 
-        public int RollNo
+        public static void TestMethod()
         {
-            get;
-            set;
-        }
+            Assembly myAssembly = Assembly.LoadFile("/Users/nicolaiwulff/RiderProjects/CsExam/CsExam/bin/Debug/netcoreapp3.1/AssemblyExample.dll");
+            var myType = myAssembly.GetType("AssemblyExample.ClassExample");
+            dynamic objMyClass = Activator.CreateInstance(myType);
+            Type parameterType = objMyClass.GetType();
 
-        public string Name
-        {
-            get;
-            set;
-        }
-
-        // No Argument Constructor 
-        public Type_Reflection()
-        {
-            RollNo = 0;
-            Name = string.Empty;
-        }
-
-        // Parameterised Constructor 
-        public Type_Reflection(int rno, string n)
-        {
-            RollNo = rno;
-            Name = n;
-        }
-
-        // Method to Display Data 
-        public void displayData()
-        {
-            Console.WriteLine("Roll Number : {0}", RollNo);
-            Console.WriteLine("Name : {0}", Name);
+            //Vi kan fx printe navnene på alle members af AssemblyExample
+            foreach (var memberInfo in parameterType.GetMembers())
+            {
+                Console.WriteLine(memberInfo.Name);
+            }
+            
+            //Her invokerer vi en metode fra AssemblyExample klassen. Det sker dynamisk ved runtime.
+            var res = parameterType.InvokeMember("MessageFromAssembly", BindingFlags.InvokeMethod, null, objMyClass, new Object[]{"Nicolai"});
+            Console.WriteLine(res);     
         }
     }
-
-    //class Tester
-    //{
-
-    //    //// Main Method 
-    //    //static void Main(string[] args)
-    //    //{
-    //    //    // Declare Instance of class Assembly 
-    //    //    // Call the GetExecutingAssembly method 
-    //    //    // to load the current assembly 
-    //    //    Assembly executing = Assembly.GetExecutingAssembly();
-
-    //    //    // Array to store types of the assembly 
-    //    //    Type[] types = executing.GetTypes();
-    //    //    foreach (var item in types)
-    //    //    {
-    //    //        // Display each type 
-    //    //        Console.WriteLine("Class : {0}", item.Name);
-
-    //    //        // Array to store methods 
-    //    //        MethodInfo[] methods = item.GetMethods();
-    //    //        foreach (var method in methods)
-    //    //        {
-    //    //            // Display each method 
-    //    //            Console.WriteLine("--> Method : {0}", method.Name);
-
-    //    //            // Array to store parameters 
-    //    //            ParameterInfo[] parameters = method.GetParameters();
-    //    //            foreach (var arg in parameters)
-    //    //            {
-    //    //                // Display each parameter 
-    //    //                Console.WriteLine("----> Parameter : {0} Type : {1}",
-    //    //                                        arg.Name, arg.ParameterType);
-    //    //            }
-    //    //        }
-    //    //    }
-    //    //}
-    //}
 }
